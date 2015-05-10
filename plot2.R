99f7e29c3fea2d5134a9c7c9ac04d4e965f1edb2
Yin Tan <itanyinmath@gmail.com>
2015-05-10 18:27:58 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,17 @@
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+        dir.create('plots')
+}
+
+# load data
+source("get_and_clean_data.R")
+
+# open device
+png(filename='plots/plot2.png',width=480,height=480,units='px')
+
+# plot data
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+# close device
+x<-dev.off()
